49675d17762d131f1a4312605ffcc76f950c3a1f
Cathy Chen <cathychen2357@gmail.com>
2019-02-18 18:16:29 +0100

    Remove word `Random` from plot titles.

--- slurms/generate_accuracy_plots.sh
+++ slurms/generate_accuracy_plots.sh
@@ -21,5 +21,5 @@ source activate thesis
 module load cudnn/cuda-9.0/7.0.3
 module load cudatoolkit/9.0
 
-python /home/cc27/Thesis/generalized_schema_learning/analysis/generate_accuracy_plots.py --exp_folder=variablefiller_gensymbolicstates_100000_1_testunseen_AllQs/variable_filler/ --exp_title="Variable Random Representation" --exp_title2="AllQs, Previously Unseen Fillers" --trial_num=0 --split_queries="QDessert,QDrink,QEmcee,QFriend,QPoet,QSubject"
-python /home/cc27/Thesis/generalized_schema_learning/analysis/generate_accuracy_plots.py --exp_folder=variablefiller_gensymbolicstates_100000_1_testunseen_QSubject/variable_filler/ --exp_title="Variable Random Representation" --exp_title2="QSubject, Previously Unseen Fillers" --trial_num=0
+python /home/cc27/Thesis/generalized_schema_learning/analysis/generate_accuracy_plots.py --exp_folder=variablefiller_gensymbolicstates_100000_1_testunseen_AllQs/variable_filler/ --exp_title="Variable Representation" --exp_title2="AllQs, Previously Unseen Fillers" --trial_num=0 --split_queries="QDessert,QDrink,QEmcee,QFriend,QPoet,QSubject"
+python /home/cc27/Thesis/generalized_schema_learning/analysis/generate_accuracy_plots.py --exp_folder=variablefiller_gensymbolicstates_100000_1_testunseen_QSubject/variable_filler/ --exp_title="Variable Representation" --exp_title2="QSubject, Previously Unseen Fillers" --trial_num=0
