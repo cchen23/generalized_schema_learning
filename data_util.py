8fbe20c8fb9f1f68613c67e3514a829a9bf40ddc
cchen23 <cathychen2357@gmail.com>
2020-01-25 15:19:17 -0500

    Update data_util.py for robustness tests and probing for statistics.

--- data_util.py
+++ data_util.py
@@ -51,35 +51,39 @@ def generate_batch(X, y, FLAGS, embedding):
         start_index = batch_num * batch_size
         end_index = min((batch_num + 1) * batch_size, data_size)
         if filler_type == "fixed_filler":
-            yield embedding[shift_inputs(X[start_index:end_index].squeeze(), FLAGS.experiment_name)], embedding[y[start_index:end_index].squeeze()], embedding
+            if FLAGS.function not in ["analyze", "probe_statistics"]:
+                yield embedding[shift_inputs(X[start_index:end_index].squeeze(), FLAGS.experiment_name)], embedding[y[start_index:end_index].squeeze()], embedding
+            else:
+                yield embedding[X[start_index:end_index].squeeze()], embedding[y[start_index:end_index].squeeze()], embedding
         elif filler_type == "variable_filler":
             # NOTE: Filler indices manually determined using word list saved by experiment creators.
             if FLAGS.experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_QSubject":
                 FILLER_INDICES = [0, 5, 6, 13, 17, 18, 19, 20, 21, 22, 23, 24]
-            elif FLAGS.experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_QSubjectQFriend":
-                FILLER_INDICES = [0, 5, 6, 13, 18, 19, 20, 21, 22, 23, 24, 25]
             elif FLAGS.experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_AllQs":
                 FILLER_INDICES = [0, 6, 7, 14, 19, 20, 24, 25, 26, 27, 28, 29]
+            elif FLAGS.experiment_name == "storyv2_train20000_AllQs":
+                FILLER_INDICES = [16, 12, 14, 17, 6, 8]
             else:
                 raise ValueError("Unsupported experiment name")
             DIMS = 50
             batchX, batchy = X[start_index:end_index].squeeze(), y[start_index:end_index].squeeze()
-            if FLAGS.function != "analyze": # Don't randomly shift inputs for decoding analysis.
+            if FLAGS.function not in ["analyze", "probe_statistics"]: # Don't randomly shift inputs for decoding analysis.
+                print("flag function", FLAGS.function)
                 batchX = shift_inputs(batchX, FLAGS.experiment_name)
             embeddingX, embeddingy = embedding[batchX], embedding[batchy]
             epoch_embedding = embedding
-            for examplenum in range(batch_size):
+            for example_num in range(batch_size):
                 # Create new random embedding for each filler.
                 num_fillers = len(FILLER_INDICES)
                 new_filler_embedding = np.empty((num_fillers, DIMS))
                 for j in range(num_fillers):
                     new_filler_embedding[j,:] = embedding_util.create_word_vector()
                 # Replace filler embedding with new random embedding.
-                filler_ix_X = np.where(np.isin(batchX[examplenum], FILLER_INDICES))
-                new_embedding_ix_X = [FILLER_INDICES.index(i) for i in batchX[examplenum,filler_ix_X][0]]
-                embeddingX[examplenum,filler_ix_X] = new_filler_embedding[new_embedding_ix_X]
-                new_embedding_ix_y = [FILLER_INDICES.index(batchy[examplenum])]
-                embeddingy[examplenum] = new_filler_embedding[new_embedding_ix_y]
+                filler_ix_X = np.where(np.isin(batchX[example_num], FILLER_INDICES))
+                new_embedding_ix_X = [FILLER_INDICES.index(i) for i in batchX[example_num,filler_ix_X][0]]
+                embeddingX[example_num,filler_ix_X] = new_filler_embedding[new_embedding_ix_X]
+                new_embedding_ix_y = [FILLER_INDICES.index(batchy[example_num])]
+                embeddingy[example_num] = new_filler_embedding[new_embedding_ix_y]
                 # Append embedding to original embedding identifying response.
                 epoch_embedding = np.concatenate((epoch_embedding, new_filler_embedding), axis=0)
             yield embeddingX, embeddingy, epoch_embedding
@@ -88,16 +92,26 @@ def shift_inputs(batchX, experiment_name):
     # NOTE: Padding indices manually determined using word list saved by experiment creators.
     if experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_QSubject":
         padding_index = 15
-    elif experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_QSubjectQFriend":
-        padding_index = 15
     elif experiment_name == "variablefiller_gensymbolicstates_100000_1_testunseen_AllQs":
         padding_index = 16
     elif experiment_name == "fixedfiller_gensymbolicstates_100000_1_AllQs":
-        padding_index = 25
+        padding_index = 27
     elif experiment_name == "fixedfiller_gensymbolicstates_100000_1_testunseen_AllQs":
-        padding_index = 25
-    elif experiment_name == "fixedfiller_gensymbolicstates_100000_1_testunseen_QSubject":
-        padding_index = 24
+        padding_index = 27
+    elif experiment_name == "storyv2_train20000_AllQs":
+        padding_index = 18
+    elif experiment_name == "generate_train3roles_testnewrole_10personspercategory_24000train_120test":
+        padding_index = 58
+    elif experiment_name == "generate_train3roles_testnewrole_100personspercategory_24000train_120test":
+        padding_index = 418
+    elif experiment_name == "generate_train3roles_testnewrole_1000personspercategory_24000train_120test":
+        padding_index = 4018
+    elif experiment_name == "generate_train3roles_testnewrole_withunseentestfillers_10personspercategory_24000train_120test":
+        padding_index = 158
+    elif experiment_name == "generate_train3roles_testnewrole_withunseentestfillers_100personspercategory_24000train_120test":
+        padding_index = 518
+    elif experiment_name == "generate_train3roles_testnewrole_withunseentestfillers_1000personspercategory_24000train_120test":
+        padding_index = 4118
     else:
         raise ArgumentError("Unsupported experiment name.")
     batch_size = batchX.shape[0]
