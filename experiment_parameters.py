7f8767481f96f080754e8ef80a260e57fb4b1bc7
cchen23 <cathychen2357@gmail.com>
2020-07-26 22:17:45 -0400

    update experiment parameters.

--- experiment_parameters.py
+++ experiment_parameters.py
@@ -10,7 +10,10 @@ input_dims = {
     "variablefiller_gensymbolicstates_100000_1_testunseen_AllQs":29,
     "variablefiller_gensymbolicstates_100000_1_testunseen_QSubject":29,
     "probe_role_statistic_recall":15,
-    "probe_role_statistic_recall_with_context":16,
+    "probe_role_statistic_recall_normalize":15,
+    "probe_role_statistic_recall_normalize_90":15,
+    "probe_role_statistic_recall_normalize_75":15,
+    "probe_role_statistic_recall_normalize_100":15,
     "fixedfiller_gensymbolicstates_100000_1_testunseen_QSubject":27,
     "variablefiller_gensymbolicstates_100000_1_testunseen_QSubjectQFriend":29,
 }
@@ -20,7 +23,10 @@ corpus_size = {
     "variablefiller_gensymbolicstates_100000_1_testunseen_AllQs":31,
     "variablefiller_gensymbolicstates_100000_1_testunseen_QSubject":26,
     "probe_role_statistic_recall":5018,
-    "probe_role_statistic_recall_with_context":5020,
+    "probe_role_statistic_recall_normalize":5018,
+    "probe_role_statistic_recall_normalize_75":23018,
+    "probe_role_statistic_recall_normalize_90":23018,
+    "probe_role_statistic_recall_normalize_100":23018,
 
     "fixedfiller_gensymbolicstates_100000_1_testunseen_QSubject":39,
     "variablefiller_gensymbolicstates_100000_1_testunseen_QSubjectQFriend":27,
