6c2ae59491252b9335610723e0b7f38bd29ab9f3
cchen23 <cathychen2357@gmail.com>
2021-02-19 00:32:18 -0500

    Remove extraneous files.

--- slurms/run_run_on_cluster.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system('python run_on_cluster.py --experiment-name variablefiller_AllQs --filler-type variable_filler_distributions_5050_AB_noise --model-names RNN-LN-FW NTM2-xl --test-filenames test_QDESSERT_double.p test_QDRINK.p test_QEMCEE_double.p test_QFRIEND.p test_QPOET.p test_QSUBJECT.p --trial-nums 40 41 42 43 44 --function probe_ambiguous --checkpoint-filler-type variable_filler_distributions')
